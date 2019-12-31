b43376d9e9c3b67333e68c31815a876b2a6db09c
davydka <davydka@gmail.com>
2019-12-30 20:30:20 -0800

    fix: test out layout thrashing on build

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import './src/styles/fonts.scss'
 import './src/styles/resets.scss'
 import './src/styles/defaults.scss'
@@ -18,3 +20,12 @@ export const onRouteUpdate = ({ location, prevLocation }) => {
 export const shouldUpdateScroll = () => {
   return false
 }
+
+/* eslint-disable */
+export const onClientEntry = () => {
+  if (process.env.NODE_ENV !== 'production') {
+    const whyDidYouRender = require('@welldone-software/why-did-you-render')
+    whyDidYouRender(React)
+  }
+}
+/* eslint-enable */
--- src/components/SectionCover/index.js
+++ src/components/SectionCover/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, createRef, forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames/bind'
 import { connect } from 'react-redux'
@@ -13,26 +13,30 @@ import Pause from '@components/Logo/Pause'
 const cx = classnames.bind(styles)
 
 const SectionCover = ({ className, showBorders }) => {
-  const [selected, setSelected] = useState(false)
+  const [selectedSKU, setSelectedSKU] = useState(false)
+  // const handleClick = sku => {
+  //   setSelectedSKU(sku)
+  // }
 
-  const Cover = ({ sku, type, image }) => {
+  const Cover = forwardRef(({ sku, type, image /*selected*/ }, ref) => {
     return (
-      <div className={cx(type, { playing: sku === selected })}>
+      <div ref={ref} className={cx(type, { playing: sku === selectedSKU })}>
         <Link to="/" className={cx('')}>
-          <img src={`/moon-glyph/images/${type}/${image}.png`} alt={type} />
+          <img src={`/moon-glyph/images/${type}/${image}.png`} alt={type} height={1400} width={840} />
+          {/*<img src={cassette01} alt={type} height={1400} width={840} />*/}
         </Link>
 
-        {sku !== selected && (
-          <button className={cx('play-pause')} onClick={() => setSelected(sku)}>
-            <Play />
-          </button>
-        )}
+        {/*{!selected && (*/}
+        <button className={cx('play-pause', 'play')} onClick={() => setSelectedSKU(sku)}>
+          <Play />
+        </button>
+        {/*)}*/}
 
-        {sku === selected && (
-          <button className={cx('play-pause')} onClick={() => setSelected(false)}>
-            <Pause />
-          </button>
-        )}
+        {/*{selected && (*/}
+        <button className={cx('play-pause', 'pause')} onClick={() => setSelectedSKU(false)}>
+          <Pause />
+        </button>
+        {/*)}*/}
 
         <Link to="/" className={cx('content')}>
           <Text tag="h4" type="h4" className={cx('band-name')}>
@@ -45,13 +49,47 @@ const SectionCover = ({ className, showBorders }) => {
         </Link>
       </div>
     )
-  }
+  })
+
+  Cover.displayName = 'Cover'
 
   Cover.propTypes = {
     sku: PropTypes.string,
     type: PropTypes.string,
+    selected: PropTypes.bool,
     image: PropTypes.string,
   }
+
+  const itemsArray = [
+    { sku: 'mg001', type: 'cassette', image: '01' },
+    { sku: 'mg002', type: 'cassette', image: '02' },
+    { sku: 'mg003', type: 'cassette', image: '03' },
+    { sku: 'mg004', type: 'cassette', image: '04' },
+    { sku: 'mg005', type: 'cassette', image: '05' },
+
+    { sku: 'mg006', type: 'vinyl', image: '01' },
+    { sku: 'mg007', type: 'vinyl', image: '02' },
+    { sku: 'mg008', type: 'vinyl', image: '03' },
+
+    { sku: 'mg009', type: 'cassette', image: '06' },
+    { sku: 'mg010', type: 'vinyl', image: '04' },
+    { sku: 'mg011', type: 'cassette', image: '07' },
+    { sku: 'mg012', type: 'cassette', image: '08' },
+  ]
+
+  const elRef = useRef([...Array(itemsArray.length)].map(() => createRef()))
+  const items = itemsArray.map((item, index) => {
+    return (
+      <Cover
+        ref={elRef.current[index]}
+        key={index}
+        sku={item.sku}
+        type={item.type}
+        image={item.image}
+        // selected={item.sku === selectedSKU}
+      />
+    )
+  })
 
   return (
     <div className={`section-container ${cx('section-cover', className, { borders: showBorders })}`}>
@@ -59,20 +97,21 @@ const SectionCover = ({ className, showBorders }) => {
         <div className={`row`}>
           <div className={`col`}>
             <div className={cx('content-container')}>
-              <Cover sku="mg001" type="cassette" image="01" />
-              <Cover sku="mg002" type="cassette" image="02" />
-              <Cover sku="mg003" type="cassette" image="03" />
-              <Cover sku="mg004" type="cassette" image="04" />
-              <Cover sku="mg005" type="cassette" image="05" />
+              {items}
+              {/*<Cover sku="mg001" type="cassette" image="01" />*/}
+              {/*<Cover sku="mg002" type="cassette" image="02" />*/}
+              {/*<Cover sku="mg003" type="cassette" image="03" />*/}
+              {/*<Cover sku="mg004" type="cassette" image="04" />*/}
+              {/*<Cover sku="mg005" type="cassette" image="05" />*/}
 
-              <Cover sku="mg006" type="vinyl" image="01" />
-              <Cover sku="mg007" type="vinyl" image="02" />
-              <Cover sku="mg008" type="vinyl" image="03" />
+              {/*<Cover sku="mg006" type="vinyl" image="01" />*/}
+              {/*<Cover sku="mg007" type="vinyl" image="02" />*/}
+              {/*<Cover sku="mg008" type="vinyl" image="03" />*/}
 
-              <Cover sku="mg009" type="cassette" image="06" />
-              <Cover sku="mg010" type="vinyl" image="04" />
-              <Cover sku="mg011" type="cassette" image="07" />
-              <Cover sku="mg012" type="cassette" image="08" />
+              {/*<Cover sku="mg009" type="cassette" image="06" />*/}
+              {/*<Cover sku="mg010" type="vinyl" image="04" />*/}
+              {/*<Cover sku="mg011" type="cassette" image="07" />*/}
+              {/*<Cover sku="mg012" type="cassette" image="08" />*/}
             </div>
           </div>
         </div>
@@ -88,6 +127,10 @@ SectionCover.propTypes = {
 
 const mapStateToProps = ({ showBorders }) => {
   return { showBorders }
+}
+
+SectionCover.whyDidYouRender = {
+  logOnDifferentValues: true,
 }
 
 export default connect(mapStateToProps)(SectionCover)
